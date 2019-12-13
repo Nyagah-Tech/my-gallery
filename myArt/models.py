192391a849119dced79cd298b95d09d0f66a74ab
dan <dan@localhost.localdomain>
2019-12-13 12:29:00 +0300

    create the image class that is responsible for creating our images and saving the images in the db

--- myArt/models.py
+++ myArt/models.py
@@ -5,3 +5,10 @@ class Owner(models.Model):
     first_name = models.CharField(max_length = 50)
     last_name = models.CharField(max_length = 50)
     email = model.EmailField()
+
+class Gallery(models.Model):
+    description = models.TextField()
+    name = models.CharField(max_length = 50)
+    owner = models.ForeignKey(Owner, on_delete = models.CACSCADE)
+    picture = models.ImageField()
+    
