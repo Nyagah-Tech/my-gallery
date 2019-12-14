eaca442c243c1ef86f6ad3c762ea14e426b4fa26
dan <dan@localhost.localdomain>
2019-12-14 13:38:04 +0300

    register bootstrap4 to the application settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'myArt.apps.MyartConfig'
+    'myArt.apps.MyartConfig',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
