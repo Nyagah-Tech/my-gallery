f9a95e08143cb361cf057fb9476452b4296ea668
dan <dan@localhost.localdomain>
2019-12-14 13:44:45 +0300

    create view function that renders our homepage and that displays 1 image

--- myArt/views.py
+++ myArt/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse,Http404
+import datetime as dt
+from .models import Gallery
+
 
 # Create your views here.
+
+def landing_page(request):
+    date = dt.date.today()
+    images = Gallery.get_all_images()
+
+    return render(request,'display-images/landing.html',{"date":date,"images":images})
+
+def image_section(request,image_id):
+    image =Gallery.get_image_by_id(image_id)
+
+    return render(request, 'display-images/image.html',{"image":image} )
