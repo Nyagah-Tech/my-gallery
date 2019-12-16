e53d373c9cc31ca2a87c3443f057da22f7451243
dan <dan@localhost.localdomain>
2019-12-16 12:28:30 +0300

    replaced the urls with the path method

--- myArt/urls.py
+++ myArt/urls.py
@@ -1,13 +1,15 @@
-from django.conf.urls import url
+from django.urls import path
 from  . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns=[
-    url(r'^$',views.landing_page, name ='landingPage'),
-    url(r'^images/(\d+)',views.image_section,name = 'image-section')
-    
+    path('',views.landing_page, name ='landingPage'),
+    path('images/<int:id>',views.image_section,name = 'image-section'),
+    path('category/<int:id>' ,views.category,name ='category'),
+    path('location/<int:id>',views.location,name='location'),
+    path('search/',views.search_term,name="results")
 ]
 
 if settings.DEBUG:
