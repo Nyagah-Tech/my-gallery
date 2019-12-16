99307de3b82da2e7343337da86d97a9a8a70d526
dan <dan@localhost.localdomain>
2019-12-16 15:23:53 +0300

    deploy

--- gallery/settings.py
+++ gallery/settings.py
@@ -33,16 +33,16 @@ if config('MODE')=="dev":
    }
 # production
 else:
-   DATABASES = {
+    DATABASES = {
        'default': dj_database_url.config(
            default=config('DATABASE_URL')
-       )
-   }
+        )
+    }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+    db_from_env = dj_database_url.config(conn_max_age=500)
+    DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+    ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
