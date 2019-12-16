c313409a5ab2551a970e3a36e4629c4f828c2a68
dan <dan@localhost.localdomain>
2019-12-16 12:33:40 +0300

    create a view function that searches for a single image and renders it

--- myArt/views.py
+++ myArt/views.py
@@ -14,3 +14,9 @@ def landing_page(request):
     category = Category.get_all_categories()
     return render(request,'display-images/landing.html',{"date":date,"images":images,"category":category,"locations":locations})
 
+def image_section(request,id):
+    image =Gallery.get_image_by_id(id)
+    locations = Location.get_all_locations()
+    category = Category.get_all_categories()
+
+    return render(request, 'display-images/image.html',{"image":image,"category":category,"locations":locations})
